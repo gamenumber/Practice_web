fd283215e3b62aa746d6137eb3d4f641e00a70bf
gamenumber <kimhunbin0226@gmail.com>
2024-06-13 13:04:20 +0900

    세로형 다시하기

---
+++ 1시간에 세로 완성하기/script/script.js
@@ -0,0 +1,17 @@
+// 네비게이션
+
+$('.menu > li').mouseover(function(){
+    $(this).children('.sub').stop().slideDown()
+})
+$('.menu > li').mouseout(function(){
+    $(this).children('.sub').stop().slideUp()
+})
+
+// 팝업
+
+$('.op-pop').click(function(){
+    $('.pop').fadeIn()
+})
+$('.cl-pop').click(function(){
+    $('.pop').fadeOut()
+})
